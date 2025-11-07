a09a429bc1fb9e03aed677e09823c8a7b942f528
RLucas1102 <robenoltl@gmail.com>
2025-11-07 12:27:53 -0500

    Able to move on particle from the center to the right side of the screen

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import noise
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QMenuBar, QMenu, QFileDialog, QVBoxLayout, QComboBox, QWidget, QSlider 
 from PySide6.QtGui import QPixmap, QImage, QColor, QAction
-from PySide6.QtCore import Qt, Signal
+from PySide6.QtCore import Qt, Signal, QTimer
 
 class ParticleWindow(QMainWindow):
     def __init__(self):
@@ -17,19 +17,14 @@ class ParticleWindow(QMainWindow):
         # Share Numpy arrays for efficiency: xcoords, xycoords
         self.npImgCont = np.zeros((self.dim, self.dim), dtype=np.float32)
 
-        x = 256
-        y = 256
+        self.v = 1
 
-        p = np.array([[x],[y]])
+        self.x = 256
+        self.y = 256
 
-        self.npImgCont[p[0], p[1]] = 1.0
+        self.p = np.array([[self.x],[self.y]])
 
-        # Linearly divide range between 0 and 1 to make np array
-        # self.xcoords = np.linspace(0, 1, self.dim)
-
-        # Make similar structure in 2D similar to UV coords
-        # x, y = np.meshgrid(self.xcoords, self.xcoords)
-        # self.xycoords = np.dstack((x, y))
+        self.npImgCont[self.p[0], self.p[1]] = 1
 
         # Set window title and size
         self.setWindowTitle("Particle System")
@@ -40,13 +35,37 @@ class ParticleWindow(QMainWindow):
         self.imageLabel.setMinimumSize(1, 1)
         self.setCentralWidget(self.imageLabel)
 
-        # Update npImgCont by broadcasting fcn output (y) over all of the rows of npImgCont (which should be 1s)
-        # np.multiply(self.npImgCont, x, out=self.npImgCont)
+        # Update pixmap by converting range, to QImage, and setting imageLabel
+        fImageData = (self.npImgCont * 255).astype(np.uint8)
+        self.pixmap = QPixmap.fromImage(QImage(fImageData.astype(np.uint8).data, fImageData.shape[1], fImageData.shape[0], QImage.Format_Grayscale8))
+        self.imageLabel.setPixmap(self.pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio))
+
+        self.timer = QTimer(self)
+
+        self.timer.timeout.connect(self.update)
+
+        self.timer.start(10)
+
+    def update(self):
+
+        if self.x >= 511:
+            self.x = 0
+
+        # start fresh instead of modifying already drawn image
+        self.npImgCont[:, :] = 0 
+
+        self.x += self.v
+
+        self.p = np.array([[self.x], [self.y]])
+
+        self.npImgCont[self.p[1], self.p[0]] = 1
 
         # Update pixmap by converting range, to QImage, and setting imageLabel
         fImageData = (self.npImgCont * 255).astype(np.uint8)
         self.pixmap = QPixmap.fromImage(QImage(fImageData.astype(np.uint8).data, fImageData.shape[1], fImageData.shape[0], QImage.Format_Grayscale8))
         self.imageLabel.setPixmap(self.pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio))
+
+
 
 app = QApplication()
 
