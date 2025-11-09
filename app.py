04415d979a020a35020a1dc23dee5bea9984f97a
RLucas1102 <robenoltl@gmail.com>
2025-11-09 09:46:11 -0500

    Added vectors it move particles, but crashes because too many leave the screen

--- app.py
+++ app.py
@@ -26,9 +26,18 @@ class ParticleWindow(QMainWindow):
 
         np.add(positions, self.dim/4, out=positions)
 
-        positions = positions.astype(int)
+        self.positions_out = positions.astype(int)
 
-        
+        # Create random vectors
+        choices = np.array([-1, 0, 1])
+
+        vectorx = np.random.choice(choices, num_particles)
+
+        vectory = np.random.choice(choices, num_particles)
+
+        vectors = np.dstack((vectorx, vectory))
+
+        self.vectors_out = vectors[0]
 
         # Set window title and size
         self.setWindowTitle("Particle System")
@@ -39,13 +48,14 @@ class ParticleWindow(QMainWindow):
         self.imageLabel.setMinimumSize(1, 1)
         self.setCentralWidget(self.imageLabel)
 
-        np.add.at(self.npImgCont, (positions[:, 0], positions[:, 1]), 1)
+        np.add.at(self.npImgCont, (self.positions_out[:, 0], self.positions_out[:, 1]), 1)
 
         # Update pixmap by converting range, to QImage, and setting imageLabel
         fImageData = (self.npImgCont * 255).astype(np.uint8)
         self.pixmap = QPixmap.fromImage(QImage(fImageData.astype(np.uint8).data, fImageData.shape[1], fImageData.shape[0], QImage.Format_Grayscale8))
         self.imageLabel.setPixmap(self.pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio))
 
+        # Create timer for constant updates
         self.timer = QTimer(self)
 
         self.timer.timeout.connect(self.update)
@@ -53,7 +63,21 @@ class ParticleWindow(QMainWindow):
         self.timer.start(10)
 
     def update(self):
-        pass
+
+        self.npImgCont[:, :] = 0
+
+        np.add(self.positions_out, self.vectors_out, out=self.positions_out)
+
+        np.add.at(self.npImgCont, (self.positions_out[:, 0], self.positions_out[:, 1]), 1)
+
+        # Update pixmap by converting range, to QImage, and setting imageLabel
+        fImageData = (self.npImgCont * 255).astype(np.uint8)
+        self.pixmap = QPixmap.fromImage(QImage(fImageData.astype(np.uint8).data, fImageData.shape[1], fImageData.shape[0], QImage.Format_Grayscale8))
+        self.imageLabel.setPixmap(self.pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio))
+
+
+
+
 
 
 
