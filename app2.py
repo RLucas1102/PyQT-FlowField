bf7898bfe0d2f1a343bb4fb2e9576c64df0120b2
RLucas1102 <robenoltl@gmail.com>
2025-11-21 16:01:44 -0500

    Added a lot of functionality to the GUI and the user can interact with the flow

--- app2.py
+++ app2.py
@@ -5,9 +5,9 @@ import noise
 
 from PySide6.QtWidgets import ( QApplication, QMainWindow, QLabel, QMenuBar, QMenu, 
                                 QFileDialog, QVBoxLayout, QComboBox, QWidget, QSlider, 
-                                QVBoxLayout, QPushButton, QHBoxLayout,)
+                                QVBoxLayout, QPushButton, QHBoxLayout, QLineEdit,)
 from PySide6.QtGui import QPixmap, QImage, QColor, QAction
-from PySide6.QtCore import Qt, Signal, QTimer
+from PySide6.QtCore import Qt, Signal, QTimer, QSize
 
 class Particles():
     def __init__(self, num, windowSize):
@@ -23,8 +23,8 @@ class Particles():
         # Keep in range 512, 512
         self.positions *= self.dim
 
-        # Create placeholder array for vectors and flow field
-        self.vectors = None
+        # Save original positions for updates
+        self.origPositions = np.copy(self.positions)
 
         self.flowField = None
 
@@ -32,6 +32,7 @@ class Particles():
         self.zoom = 0.1
         self.curve = 3
         self.cellSize = 20
+        self.currCellSize = self.cellSize # Keeps cell size in this class
 
         # Initialize screen to a beautiful image
         self.generateSymmetricFlow()
@@ -44,30 +45,32 @@ class Particles():
 
         self.positions = np.concatenate((self.positions, newPosition))
 
-        newVector = np.random.rand(1, 2)
+    def generateRandomFlow(self):
 
-        newVector = newVector - 0.5
-
-        self.vectors = np.concatenate((self.vectors, newVector))
-
-    def updateParticlesRand(self):
-
-        # Add vector to position
-        self.positions[:, 0] += self.vectors[:, 0] + np.random.rand(1) * 2 - 1
-        self.positions[:, 1] += self.vectors[:, 1] + np.random.rand(1) * 2 - 1
-
-    def generateRandomVectors(self):
-
-        # Create array of random particle vectors (keep [0,1])
-        self.vectors = np.random.rand(self.num_particles, 2)
-
-        self.vectors = self.vectors - 0.5
-    
-    def generateSymmetricFlow(self):
+        # Set new cell size
+        self.currCellSize = self.cellSize
 
         # Create flow field
-        self.rows = int(np.floor(self.dim / self.cellSize))
-        self.cols = int(np.floor(self.dim / self.cellSize))
+        self.rows = int(np.floor(self.dim / self.currCellSize))
+        self.cols = int(np.floor(self.dim / self.currCellSize))
+
+        # This flow field grid will hold our angles for particles to access
+        self.flowField = np.zeros((self.rows, self.cols), dtype=np.float32)
+
+        # Add angles to each position within the flow field
+        for y in range(self.rows):
+            for x in range(self.cols):
+                angle = ((np.random.rand(1) - 0.5) * np.pi)
+                self.flowField[y][x] = angle
+    
+    def generateSymmetricFlow(self):
+        
+        # Set new cell size
+        self.currCellSize = self.cellSize
+
+        # Create flow field
+        self.rows = int(np.floor(self.dim / self.currCellSize))
+        self.cols = int(np.floor(self.dim / self.currCellSize))
 
         # This flow field grid will hold our angles for particles to access
         self.flowField = np.zeros((self.rows, self.cols), dtype=np.float32)
@@ -84,8 +87,8 @@ class Particles():
         xInGrid = np.zeros(self.positions.shape[0], dtype=np.int32)
         yInGrid = np.zeros(self.positions.shape[0], dtype=np.int32)
         
-        xInGrid[:] = np.floor(self.positions[:,0] / self.cellSize)
-        yInGrid[:] = np.floor(self.positions[:,1] / self.cellSize)
+        xInGrid[:] = np.floor(self.positions[:,0] / self.currCellSize)
+        yInGrid[:] = np.floor(self.positions[:,1] / self.currCellSize)
 
         xInGrid %= self.cols
         yInGrid %= self.rows
@@ -103,6 +106,18 @@ class Particles():
 
     def getParticles(self):
         return self.positions
+    
+    def resetParticles(self):
+        self.positions = np.copy(self.origPositions)
+    
+    def setZoom(self, zoomVal):
+        self.zoom = zoomVal
+
+    def setCurve(self, curveVal):
+        self.curve = curveVal
+
+    def setCellSize(self, cellsVal):
+        self.cellSize = cellsVal
 
 class ParticleWindow(QMainWindow):
     def __init__(self):
@@ -141,8 +156,8 @@ class ParticleWindow(QMainWindow):
 
         # Layouts
         # --------------------------------------
-        mainLayout = QHBoxLayout()
-        toolLayout = QVBoxLayout()
+        mainLayout = QVBoxLayout()
+        toolLayout = QHBoxLayout()
 
         # Labels
         # --------------------------------------
@@ -151,17 +166,107 @@ class ParticleWindow(QMainWindow):
         self.imageLabel = QLabel()
         self.imageLabel.setMinimumSize(1, 1)
 
+        # Zoom label
+        self.zoomLabel = QLabel()
+        self.zoomLabel.setText("Zoom:")
+        self.zoomLabel.setMinimumSize(1, 1)
+        
+        # Curve label
+        self.curveLabel = QLabel()
+        self.curveLabel.setText("Curve:")
+        self.curveLabel.setMinimumSize(1, 1)
+        
+        # Cellsize label
+        self.cellLabel = QLabel()
+        self.cellLabel.setText("Cell size:")
+        self.cellLabel.setMinimumSize(1, 1)
+
+        # Combo box
+        # --------------------------------------
+        self.flowCombo = QComboBox()
+        self.flowCombo.addItem("Random Flow")
+        self.flowCombo.addItem("Symmetric Flow")
+        self.flowCombo.setCurrentIndex(1)
+        self.flowCombo.currentIndexChanged.connect(self.genFlow)
+        self.flowCombo.setEditable(False)
+
+        # Text boxes
+        # --------------------------------------
+
+        # Zoom text box
+        self.zoomTextBox = QLineEdit("0.1", parent=self)
+        self.zoomTextBox.returnPressed.connect(self.setZoom)
+
+        # Curve text box
+        self.curveTextBox = QLineEdit("3", parent=self)
+        self.curveTextBox.returnPressed.connect(self.setCurve)
+        
+        # Cell size text box
+        self.cellSizeTextBox = QLineEdit("20", parent=self)
+        self.cellSizeTextBox.returnPressed.connect(self.setCells)
+
         # Buttons
         # --------------------------------------
-        self.randButton = QPushButton()
-        self.randButton.setText("Add Particle")
-        self.randButton.clicked.connect(self.genParticle)
+        
+        # Add particle button
+        self.particleButton = QPushButton()
+        self.particleButton.setText("Add Particle")
+        self.particleButton.clicked.connect(self.genParticle)
 
+        # Zoom Plus/Minus button
+        self.zoomPlus = QPushButton()
+        self.zoomPlus.setText("+")
+        self.zoomPlus.setFixedSize(20, 20)
+        self.zoomPlus.clicked.connect(self.addZoom)
+        
+        self.zoomMinus = QPushButton()
+        self.zoomMinus.setText("-")
+        self.zoomMinus.setFixedSize(20, 20)
+        self.zoomMinus.clicked.connect(self.subZoom)
+
+        # Curve Plus/Minus Button
+        self.curvePlus = QPushButton()
+        self.curvePlus.setText("+")
+        self.curvePlus.setFixedSize(20, 20)
+        self.curvePlus.clicked.connect(self.addCurve)
+        
+        self.curveMinus = QPushButton()
+        self.curveMinus.setText("-")
+        self.curveMinus.setFixedSize(20, 20)
+        self.curveMinus.clicked.connect(self.subCurve)
+        
+        # Cellsize Plus/Minus Button
+        self.cellsizePlus = QPushButton()
+        self.cellsizePlus.setText("+")
+        self.cellsizePlus.setFixedSize(20, 20)
+        self.cellsizePlus.clicked.connect(self.addCells)
+
+        self.cellsizeMinus = QPushButton()
+        self.cellsizeMinus.setText("-")
+        self.cellsizeMinus.setFixedSize(20,20)
+        self.cellsizeMinus.clicked.connect(self.subCells)
+        
         # Build GUI
         # --------------------------------------
-        mainLayout.addWidget(self.imageLabel)
-        toolLayout.addWidget(self.randButton)
+        toolLayout.addWidget(self.zoomLabel)
+        toolLayout.addWidget(self.zoomTextBox)
+        toolLayout.addWidget(self.zoomPlus)
+        toolLayout.addWidget(self.zoomMinus)
 
+        toolLayout.addWidget(self.curveLabel)
+        toolLayout.addWidget(self.curveTextBox)
+        toolLayout.addWidget(self.curvePlus)
+        toolLayout.addWidget(self.curveMinus)
+
+        toolLayout.addWidget(self.cellLabel)
+        toolLayout.addWidget(self.cellSizeTextBox)
+        toolLayout.addWidget(self.cellsizePlus)
+        toolLayout.addWidget(self.cellsizeMinus)
+
+        toolLayout.addWidget(self.particleButton)
+
+        mainLayout.addWidget(self.imageLabel)
+        mainLayout.addWidget(self.flowCombo)
         mainLayout.addLayout(toolLayout)
 
         dummy = QWidget()
@@ -179,6 +284,8 @@ class ParticleWindow(QMainWindow):
 
         self.drawParticles()
 
+        # This constrains the particles light intensity
+        # As they get really small, they flicker
         self.npImgCont = np.clip(self.npImgCont, 0, 1)
 
         # Update pixmap by converting range, to QImage, and setting imageLabel
@@ -206,8 +313,151 @@ class ParticleWindow(QMainWindow):
         np.add.at(self.npImgCont, (xi, yi), 1)
 
     def genParticle(self):
-
         self.particles.addParticle()
+
+    def clearImage(self):
+        self.npImgCont[:,:] = 0
+
+    def genFlow(self):
+        flowState = self.flowCombo.currentIndex()
+
+        self.particles.resetParticles()
+
+        self.clearImage()
+        
+        match flowState:
+            case 0:
+                self.particles.generateRandomFlow()
+            case 1:
+                self.particles.generateSymmetricFlow()
+            case _:
+                print("Oops something went wrong!")
+
+
+    def setZoom(self):
+        # Get value and convert to float
+        zoomstr = self.zoomTextBox.text()
+        zoomVal = float(zoomstr)
+        zoomVal = np.round(zoomVal, decimals=1)
+        self.zoomTextBox.setText(str(zoomVal))
+
+        # Set respective value in particle system
+        self.particles.setZoom(zoomVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+
+    def addZoom(self):
+        # Get value and convert to float
+        zoomStr = self.zoomTextBox.text()
+        zoomVal = float(zoomStr)
+        zoomVal += 0.1
+        zoomVal = np.round(zoomVal, decimals=1)
+        self.zoomTextBox.setText(str(zoomVal))
+
+        # Set respective value in particle system
+        self.particles.setZoom(zoomVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+    
+    def subZoom(self):
+        # Get value and convert to float
+        zoomStr = self.zoomTextBox.text()
+        zoomVal = float(zoomStr)
+        zoomVal -= 0.1
+        zoomVal = np.round(zoomVal, decimals=1)
+        self.zoomTextBox.setText(str(zoomVal))
+
+        # Set respective value in particle system
+        self.particles.setZoom(zoomVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+    
+    def setCurve(self):
+        # Get value and convert to float
+        curveStr = self.curveTextBox.text()
+        curveVal = float(curveStr)
+        curveVal = np.round(curveVal, decimals=1)
+        self.curveTextBox.setText(str(curveVal))
+
+        # Set respective value in particle system
+        self.particles.setCurve(curveVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+    
+    def addCurve(self):
+        # Get value and convert to float
+        curveStr = self.curveTextBox.text()
+        curveVal = float(curveStr)
+        curveVal += 0.1
+        curveVal = np.round(curveVal, decimals=1)
+        self.curveTextBox.setText(str(curveVal))
+        
+        # Set respective value in particle system
+        self.particles.setCurve(curveVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+    
+    def subCurve(self):
+        # Get value and convert to float
+        curveStr = self.curveTextBox.text()
+        curveVal = float(curveStr)
+        curveVal -= 0.1
+        curveVal = np.round(curveVal, decimals=1)
+        self.curveTextBox.setText(str(curveVal))
+
+        # Set respective value in particle system
+        self.particles.setCurve(curveVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+
+    def setCells(self):
+        # Get value and convert to float
+        cellsStr = self.cellSizeTextBox.text()
+        cellsVal = int(cellsStr)
+        cellsVal = np.round(cellsVal)
+        self.cellSizeTextBox.setText(str(cellsVal))
+
+        # Set respective value in particle system
+        self.particles.setCellSize(cellsVal)
+
+        # Generate new flow based on new parameters
+        self.genFlow()
+
+    def addCells(self):
+        # Get value and convert to float
+        cellsStr = self.cellSizeTextBox.text()
+        cellsVal = int(cellsStr)
+        cellsVal += 1
+        cellsVal = np.round(cellsVal)
+        self.cellSizeTextBox.setText(str(cellsVal))
+        
+        # Set respective value in particle system
+        self.particles.setCellSize(cellsVal)
+        
+        # Generate new flow based on new parameters
+        self.genFlow()
+
+    def subCells(self):
+
+        # Get value and convert to float
+        cellsStr = self.cellSizeTextBox.text()
+        cellsVal = int(cellsStr)
+        cellsVal -= 1
+        cellsVal = np.round(cellsVal)
+        self.cellSizeTextBox.setText(str(cellsVal))
+        
+        # Set respective value in particle system
+        self.particles.setCellSize(cellsVal)
+        
+        # Generate new flow based on new parameters
+        self.genFlow()
+
 
 
 app = QApplication([])
